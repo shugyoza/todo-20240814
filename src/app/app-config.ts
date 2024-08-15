d732fb14ed7e7017bb94ed8b576ff2becbf35da9
Stephen Hanjaya <stephenhanjaya@gmail.com>
2024-08-15 18:09:47 -0500

    add app-config n app-routes

---
+++ src/app/app-config.ts
@@ -0,0 +1,33 @@
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  ImportProvidersSource,
+} from '@angular/core';
+
+import { appRoutes } from './app-routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {
+  provideRouter,
+  withHashLocation,
+  withRouterConfig,
+} from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+
+const ngModuleBaseLibraries: ImportProvidersSource = [
+  /* e.g
+  someModule.forRoot({...})
+  */
+];
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(
+      appRoutes,
+      withRouterConfig({ paramsInheritanceStrategy: 'always' }),
+      withHashLocation()
+    ),
+    importProvidersFrom(...ngModuleBaseLibraries),
+    provideHttpClient(),
+    provideAnimationsAsync(),
+  ],
+};
