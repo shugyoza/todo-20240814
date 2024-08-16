52a47ca0c2c86183d498ae1a900281589bb43965
Stephen Hanjaya <stephenhanjaya@gmail.com>
2024-08-15 19:51:40 -0500

    added cookie.service
    Signed-off-by: Stephen Hanjaya <stephenhanjaya@gmail.com>

---
+++ src/app/core/services/cookie/cookie.service.ts
@@ -0,0 +1,40 @@
+import { DOCUMENT } from '@angular/common';
+import { inject, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CookieService {
+  private document = inject(DOCUMENT);
+
+  public update(update: { [key: string]: any[] | number | string }): void {
+    const cookie = { ...this.parse(), ...update };
+
+    for (const key in cookie) {
+      let value = cookie[key] ?? '';
+
+      if (Array.isArray(value)) {
+        value = value.join(',');
+      } else if (typeof value === 'number') {
+        value = value.toString();
+      } else if (typeof value === 'string') {
+        value = value.trim();
+      }
+
+      this.document.cookie = `${key}=${value};`;
+    }
+  }
+
+  public parse(): { [key: string]: string } {
+    const cookie = {
+      array: this.document.cookie?.split(';'),
+      object: {} as any,
+    };
+    cookie.array?.forEach((keyValue: string) => {
+      const [key, value] = keyValue.split('=').map((text) => text.trim());
+      cookie.object[key] = value;
+    });
+
+    return cookie.object;
+  }
+}
