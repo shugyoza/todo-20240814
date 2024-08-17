3dbb09ac7c17478be552011f917a6a50692757aa
Stephen Hanjaya <stephenhanjaya@gmail.com>
2024-08-17 00:14:16 -0500

    implement layout.pipe on app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LayoutPipe } from './shared/pipes/layout/layout.pipe';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+
+import { context } from './constants/layout.constants';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LayoutPipe, AsyncPipe, JsonPipe],
 })
 export class AppComponent {
   title = 'todo';
+  context = context;
 }
