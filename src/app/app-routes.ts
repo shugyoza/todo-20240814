490c591a98d64504089f822b12896aa4ba364bbb
Stephen Hanjaya <stephenhanjaya@gmail.com>
2024-08-19 21:23:44 -0500

    add routes

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const appRoutes: Routes = [];
+export const appRoutes: Routes = [
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./features/contacts/contacts.component').then(
+        (m) => m.ContactsComponent
+      ),
+  },
+];
