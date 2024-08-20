a2a639fd643377ff30a98d70d9a7f6d648ba6236
Stephen Hanjaya <stephenhanjaya@gmail.com>
2024-08-19 21:32:41 -0500

    add mock,clean up constants.component
    Signed-off-by: Stephen Hanjaya <stephenhanjaya@gmail.com>

--- src/app/features/contacts/contacts.component.ts
+++ src/app/features/contacts/contacts.component.ts
@@ -1,13 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { JsonPipe } from '@angular/common';
 
 import { MatDividerModule } from '@angular/material/divider';
 import { IosHeaderComponent } from 'src/app/shared/components/ios-header/ios-header.component';
 import { IosListsComponent } from 'src/app/shared/components/ios-lists/ios-lists.component';
 import { IosList } from 'src/app/shared/components/ios-lists/ios-lists.interface';
+import { lists } from 'src/assets/constants.mock';
 
 @Component({
   standalone: true,
@@ -23,66 +22,5 @@ import { IosList } from 'src/app/shared/components/ios-lists/ios-lists.interface
   ],
 })
 export class ContactsComponent {
-  public lists: IosList[] = [
-    {
-      name: 'Lists',
-      expandable: false,
-      expanded: true,
-      children: [
-        {
-          name: 'All Contacts',
-          count: 599,
-          icon: {
-            start: 'groups',
-          },
-        },
-      ],
-    },
-    {
-      name: 'steve.hanj@gmail.com',
-      expandable: true,
-      expanded: true,
-      children: [
-        {
-          name: 'All steve.hanj@gmail',
-          count: 37,
-          icon: {
-            start: 'groups',
-          },
-        },
-      ],
-    },
-    {
-      name: 'iPhone',
-      expandable: true,
-      expanded: true,
-      children: [
-        {
-          name: 'All iPhone',
-          count: 564,
-          icon: {
-            start: 'groups',
-          },
-        },
-        {
-          name: 'Client',
-          count: 1,
-          icon: {
-            start: 'group',
-          },
-        },
-        {
-          name: 'Keroppi Fans',
-          count: 2,
-          icon: {
-            start: 'group',
-          },
-        },
-      ],
-    },
-  ];
-
-  public onEdit(): void {}
-
-  public onAdd(): void {}
+  public lists: IosList[] = lists;
 }
---
+++ src/assets/constants.mock.ts
@@ -0,0 +1,58 @@
+export const lists = [
+  {
+    name: 'Lists',
+    expandable: false,
+    expanded: true,
+    children: [
+      {
+        name: 'All Contacts',
+        count: 599,
+        icon: {
+          start: 'groups',
+        },
+      },
+    ],
+  },
+  {
+    name: 'steve.hanj@gmail.com',
+    expandable: true,
+    expanded: true,
+    children: [
+      {
+        name: 'All steve.hanj@gmail',
+        count: 37,
+        icon: {
+          start: 'groups',
+        },
+      },
+    ],
+  },
+  {
+    name: 'iPhone',
+    expandable: true,
+    expanded: true,
+    children: [
+      {
+        name: 'All iPhone',
+        count: 564,
+        icon: {
+          start: 'groups',
+        },
+      },
+      {
+        name: 'Client',
+        count: 1,
+        icon: {
+          start: 'group',
+        },
+      },
+      {
+        name: 'Keroppi Fans',
+        count: 2,
+        icon: {
+          start: 'group',
+        },
+      },
+    ],
+  },
+];
